9801e027ea70100a2c92cb6b4417d56dbfdeb473
Harnick Khera <20884880+Hephyrius@users.noreply.github.com>
2021-02-21 22:45:37 +0000

    more conversions

--- React/src/utils/tronweb.js
+++ React/src/utils/tronweb.js
@@ -249,11 +249,8 @@ export async function getVoteCounters() {
         let id = "0x" + Number(pid).toString(16);
 
         //grab vote data from the blockchain
-        let upvotecall = await window.contract.methods.getUpVotes(id).call();
-        let up = window.web3.toBN(upvotecall['_hex']).toNumber();
-
-        let downvotecall = await window.contract.methods.getDownVotes(id).call();
-        let down = window.web3.toBN(downvotecall['_hex']).toNumber();
+        let up = await window.contract.methods.getUpVotes(id).call();
+        let down = await window.contract.methods.getDownVotes(id).call();
 
         let postVote = {
             postid : pid,
@@ -265,22 +262,146 @@ export async function getVoteCounters() {
     } 
 
     localStorage.setItem("PostVotes", JSON.stringify(votes));
-
 }
+
 export async function VoteOnPost(postid, votetype) {
+
+    load_contract();
+
+    //submit the data to the blockchain
+    const account = await getCurrentAccount();
+
+    //convert the postid into a useable form
+    let id = "0x" + Number(postid).toString(16);
+
+    if (votetype == 0){
+        //notify the user that the vote has been submitted
+        Swal({title:'Post Up Voted',
+        type: 'info'
+        });
+        //submit the data to the blockchain
+        await window.contract.methods.UpvotePost(postid).send({from: account})
+        .then(res => Swal({
+            title:'Up Voted Successfully',
+            type: 'success'
+
+        })).catch(err => Swal(
+            {
+                title:'Up Vote Failed',
+                type: 'error'
+            }
+        ));
+    }else if (votetype == 1){
+
+        //notify the user that the vote has been submitted
+        Swal({title:'Post Down Voted',
+        type: 'info'
+        });
+
+        //submit the data to the blockchain
+        await window.contract.methods.DownvotePost(postid).send({from: account})
+        .then(res => Swal({
+            title:'Down Voted Successfully',
+            type: 'success'
+
+        })).catch(err => Swal(
+            {
+                title:'Down Vote Failed',
+                type: 'error'
+            }
+        ));
+    }
 
 }
 
 //// Comment Related Functions
 
 //get the vote counters from the blockchain
-export async function getCommentVoteCounters() {
 
+//get the vote counters from the blockchain
+export async function getCommentVoteCounters() {
+    load_contract();
+    const account = await getCurrentAccount();
+    
+    let comments = JSON.parse(localStorage.getItem("Comments"));
+    let CommentVotes = [];
+
+    if (!comments){
+        comments = [];
+    }
+
+    for(var i=0; i<comments.length; i++){
+        let pid = comments[i]['postid'];
+        let cid = comments[i]['commentid'];
+
+        let id = "0x" + Number(pid).toString(16);
+        let comid = "0x" + Number(cid).toString(16);
+
+        //grab vote data from the blockchain
+        let up = await window.contract.methods.getCommentUpVotes(id, comid).call();
+        let down = await window.contract.methods.getCommentDownVotes(id, comid).call();
+
+        let commentVote = {
+            postid : pid,
+            commentid: cid,
+            upvotes : up,
+            downvotes: down,
+            total: (up-down)
+        }
+        CommentVotes = CommentVotes.concat(commentVote);
+    } 
+
+    localStorage.setItem("CommentVotes", JSON.stringify(CommentVotes));
 
 }
 
+
 export async function VoteOnComment(postid, commentid, votetype) {
 
+    load_contract();
+    const account = await getCurrentAccount();
+
+    //convert the postid into a useable form
+    let id = "0x" + Number(postid).toString(16);
+    let cid = "0x" + Number(commentid).toString(16);
+
+    if (votetype == 0){
+        //notify the user that the vote has been submitted
+        Swal({title:'Comment Up Voted',
+        type: 'info'
+        });
+        //submit the data to the blockchain
+        await window.contract.methods.UpvoteComment(postid, cid).send({from: account})
+        .then(res => Swal({
+            title:'Up Voted Comment Successfully',
+            type: 'success'
+
+        })).catch(err => Swal(
+            {
+                title:'Comment Vote Failed',
+                type: 'error'
+            }
+        ));
+    }else if (votetype == 1){
+
+        //notify the user that the vote has been submitted
+        Swal({title:'Comment Down Voted',
+        type: 'info'
+        });
+
+        //submit the data to the blockchain
+        await window.contract.methods.DownvoteComment(postid, cid).send({from: account})
+        .then(res => Swal({
+            title:'Comment Down Voted Successfully',
+            type: 'success'
+
+        })).catch(err => Swal(
+            {
+                title:'Comment Vote Failed',
+                type: 'error'
+            }
+        ));
+    }
 
 }
 
@@ -314,263 +435,6 @@ export async function getUsers() {
 /*
 
 
-export async function createNewComment(commentText, postid,  parentComment) {
-
-    //notify the user that the comment has been submitted
-    Swal({title:'Comment Transaction Submitted',
-            type: 'info'
-        });
-
-    //load the contract 
-    const contract = await window.web3.contract().at(contractAddress);
-
-    //convert the data to an appropriate format for the blockchain to handle
-    //let byteTitle = a2hex(title);
-    let bytecommentText = a2hex(commentText);
-    let id = "0x" + Number(postid).toString(16);
-
-    //submit the data to the blockchain
-    contract.PostComment(bytecommentText, id, "0x00").send({
-        shouldPollResponse:true,
-        callValue:0
-
-    }).then(res => Swal({
-        title:'Comment Posted Successfully',
-        type: 'success'
-
-    })).catch(err => Swal(
-        {
-             title:'Comment Post Failed',
-             type: 'error'
-        }
-    ));
-
-}
-
-//get data from contract events and convert it into a readable/useable state
-export async function getComments() {
-    tronWeb = dynamicTronlink()
-
-    //load the contract 
-    const events = await window.web3.eth.filter(contractAddress, 0, "CommentCreated", 0,  200, 1);
-
-    var comments = []
-    for(var i=0; i<events.length; i++){
-
-        let address = events[i]['result']['commenter'];
-        let tronaddress = address.substring(2, address.length);
-        tronaddress = "41" + tronaddress;
-        tronaddress = tronWeb.address.fromHex(tronaddress)
-
-        var comment = {
-            parentComment: hex2a(events[i]['result']['parentComment']),
-            postid: events[i]['result']['postId'],
-            author: address,
-            tronaddress: tronaddress,
-            content: hex2a(events[i]['result']['comment']),
-            timestamp: Time2a(events[i]['result']['commentTimestamp']),
-            commentid: events[i]['result']['commentId'],
-            hms: Time2HMS(events[i]['result']['commentTimestamp'])
-          }
-
-          comments = comments.concat(comment);
-    }
-
-    localStorage.setItem("Comments", JSON.stringify(comments));
-
-    return comments;
-}
-
-//get the vote counters from the blockchain
-export async function getVoteCounters() {
-
-    tronWeb = dynamicTronlink()
-
-    const contract = await tronWeb.contract().at(contractAddress);
-
-    let posts = JSON.parse(localStorage.getItem("Posts"));
-    let votes = [];
-
-    if (!posts){
-        posts = [];
-    }
-
-    for(var i=0; i<posts.length; i++){
-        let pid = posts[i]['postid'];
-        let id = "0x" + Number(pid).toString(16);
-
-        //grab vote data from the blockchain
-        let upvotecall = await contract.getUpVotes(id).call();
-        let up = tronWeb.toBigNumber(upvotecall['_hex']).toNumber();
-
-        let downvotecall = await contract.getDownVotes(id).call();
-        let down = tronWeb.toBigNumber(downvotecall['_hex']).toNumber();
-
-        let postVote = {
-            postid : pid,
-            upvotes : up,
-            downvotes: down,
-            total: (up-down)
-        }
-        votes = votes.concat(postVote);
-    } 
-
-    localStorage.setItem("PostVotes", JSON.stringify(votes));
-
-}
-
-export async function VoteOnPost(postid, votetype) {
-
-
-
-    //load the contract 
-    const contract = await window.web3.contract().at(contractAddress);
-
-    //convert the postid into a useable form
-    let id = "0x" + Number(postid).toString(16);
-
-    if (votetype == 0){
-        //notify the user that the vote has been submitted
-        Swal({title:'Post Up Voted',
-        type: 'info'
-        });
-        //submit the data to the blockchain
-        contract.UpvotePost(postid).send({
-            shouldPollResponse:true,
-            callValue:0
-
-        }).then(res => Swal({
-            title:'Up Voted Successfully',
-            type: 'success'
-
-        })).catch(err => Swal(
-            {
-                title:'Up Vote Failed',
-                type: 'error'
-            }
-        ));
-    }else if (votetype == 1){
-
-        //notify the user that the vote has been submitted
-        Swal({title:'Post Down Voted',
-        type: 'info'
-        });
-
-        //submit the data to the blockchain
-        contract.DownvotePost(postid).send({
-            shouldPollResponse:true,
-            callValue:0
-
-        }).then(res => Swal({
-            title:'Down Voted Successfully',
-            type: 'success'
-
-        })).catch(err => Swal(
-            {
-                title:'Down Vote Failed',
-                type: 'error'
-            }
-        ));
-    }
-
-}
-
-//// Comment Related Functions
-
-//get the vote counters from the blockchain
-export async function getCommentVoteCounters() {
-    tronWeb = dynamicTronlink()
-    const contract = await tronWeb.contract().at(contractAddress);
-
-    let comments = JSON.parse(localStorage.getItem("Comments"));
-    let CommentVotes = [];
-
-    if (!comments){
-        comments = [];
-    }
-
-    for(var i=0; i<comments.length; i++){
-        let pid = comments[i]['postid'];
-        let cid = comments[i]['commentid'];
-
-        let id = "0x" + Number(pid).toString(16);
-        let comid = "0x" + Number(cid).toString(16);
-
-        //grab vote data from the blockchain
-        let upvotecall = await contract.getCommentUpVotes(id, comid).call();
-        let up = tronWeb.toBigNumber(upvotecall['_hex']).toNumber();
-
-        let downvotecall = await contract.getCommentDownVotes(id, comid).call();
-        let down = tronWeb.toBigNumber(downvotecall['_hex']).toNumber();
-
-        let commentVote = {
-            postid : pid,
-            commentid: cid,
-            upvotes : up,
-            downvotes: down,
-            total: (up-down)
-        }
-        CommentVotes = CommentVotes.concat(commentVote);
-    } 
-
-    localStorage.setItem("CommentVotes", JSON.stringify(CommentVotes));
-
-}
-
-export async function VoteOnComment(postid, commentid, votetype) {
-
-    //load the contract 
-    const contract = await window.web3.contract().at(contractAddress);
-
-    //convert the postid into a useable form
-    let id = "0x" + Number(postid).toString(16);
-    let cid = "0x" + Number(commentid).toString(16);
-
-    if (votetype == 0){
-        //notify the user that the vote has been submitted
-        Swal({title:'Comment Up Voted',
-        type: 'info'
-        });
-        //submit the data to the blockchain
-        contract.UpvoteComment(postid, cid).send({
-            shouldPollResponse:true,
-            callValue:0
-
-        }).then(res => Swal({
-            title:'Up Voted Comment Successfully',
-            type: 'success'
-
-        })).catch(err => Swal(
-            {
-                title:'Comment Vote Failed',
-                type: 'error'
-            }
-        ));
-    }else if (votetype == 1){
-
-        //notify the user that the vote has been submitted
-        Swal({title:'Comment Down Voted',
-        type: 'info'
-        });
-
-        //submit the data to the blockchain
-        contract.DownvoteComment(postid, cid).send({
-            shouldPollResponse:true,
-            callValue:0
-
-        }).then(res => Swal({
-            title:'Comment Down Voted Successfully',
-            type: 'success'
-
-        })).catch(err => Swal(
-            {
-                title:'Comment Vote Failed',
-                type: 'error'
-            }
-        ));
-    }
-
-}
 
 //DONATION SYSTEM
 
